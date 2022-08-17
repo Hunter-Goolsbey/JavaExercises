d413234d2e5a27bb84d0c50664935d3c4395852e
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-08-16 19:28:06 -0700

    Create Main.java

---
+++ MinMax_Challenge/Main.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        public static int minAndMaxInputChallenge() {
+            while (true) {
+                //ui
+                System.out.println("Enter number");
+
+            }
+            System.out.println("Max number: " + max + " Min number is: " + min);
+        }
+    }
+}
