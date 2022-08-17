770b866c19a43a17c3c6f9957c67d76b73f55595
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-08-16 19:07:27 -0700

    Add files via upload

---
+++ Classes_Challenge/bankAccount.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+public class bankAccount {
+        public static int bankAccountone(){
+            return 1;
+        }
+        private String accountNbr;
+        private double balance;
+        private String customerName;
+        private String email;
+        private String phoneNbr;
+
+
+}
