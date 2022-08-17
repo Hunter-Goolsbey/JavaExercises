9bd03132e44a2bf88c56c81a0e04f338295c1eb0
Hunter-Goolsbey <110963084+Hunter-Goolsbey@users.noreply.github.com>
2022-08-16 19:06:48 -0700

    Update Main.java

--- Classes_Challenge/Main.java
+++ Classes_Challenge/Main.java
@@ -1,1 +1,9 @@
+package academy.learnprogramming;
 
+public class Main {
+
+    public static void main(String[] args) {
+        bankAccount cat = new bankAccount();
+        bankAccount dog = new bankAccount();
+    }
+}
